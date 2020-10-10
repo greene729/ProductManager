da741ddf49fd2b890fac8ecccdabb79ea55de870
Alex Greene <alex@roofsnap.com>
2020-10-10 11:09:34 -0400

    Initial Commit

---
+++ ProductManager/Data/EmptyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManager.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1001, Name = "Laptop", Price = 20.02, Quantity = 10, Description ="This is a best gaming laptop"},
+                new Product { Id = 1002, Name = "Microsoft Office", Price = 20.99, Quantity = 50, Description ="This is a Office Application"},
+                new Product { Id = 1003, Name = "Lazer Mouse", Price = 12.02, Quantity = 20, Description ="The mouse that works on all surface"},
+                new Product { Id = 1004, Name = "USB Storage", Price = 5.00, Quantity = 20, Description ="To store 256GB of data"}
+
+            };
+        }
+    }
+}
---
+++ ProductManager/Data/ProductService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManager.Data
+{
+    public class ProductService
+    {
+        private readonly ProductDbContext _dbContext;
+
+        public ProductService(ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> AddProductAsync(Product product)
+        {
+            try
+            {
+                _dbContext.Products.Add(product);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return product;
+        }
+
+        public async Task<Product> UpdateProductAsync(Product product)
+        {
+            try
+            {
+                var productToUpdate = _dbContext.Products.FirstOrDefault(p => p.Id == product.Id);
+                if (productToUpdate != null)
+                {
+                    _dbContext.Update(product);
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return product;
+        }
+
+        public async Task DeleteProductAsync(Product product)
+        {
+            try
+            {
+                _dbContext.Products.Remove(product);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
