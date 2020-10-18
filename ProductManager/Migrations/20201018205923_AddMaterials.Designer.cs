6cdecdf9079068a83440bf45e425554fc1c26493
Alex Greene <greene.alexanderj@gmail.com>
2020-10-18 17:00:26 -0400

    add migration for materials table

--- ProductManager/Migrations/20201018205923_AddMaterials.Designer.cs
+++ ProductManager/Migrations/20201018205923_AddMaterials.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManager.Data;
 
 namespace ProductManager.Migrations
 {
     [DbContext(typeof(ProductDbContext))]
-    partial class ProductDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201018205923_AddMaterials")]
+    partial class AddMaterials
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProductManager/Migrations/20201018205923_AddMaterials.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductManager.Migrations
+{
+    public partial class AddMaterials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1001);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1002);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1003);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1004);
+
+            migrationBuilder.CreateTable(
+                name: "Materials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Quantity = table.Column<double>(nullable: false),
+                    Units = table.Column<string>(nullable: true),
+                    Cost = table.Column<double>(nullable: false),
+                    Supplier = table.Column<string>(nullable: true),
+                    OrderableQuantity = table.Column<double>(nullable: true),
+                    SupplierItemNumber = table.Column<string>(nullable: true),
+                    ReorderLink = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materials", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Materials");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Quantity" },
+                values: new object[] { 1001, "This is a best gaming laptop", "Laptop", 20.02, 10 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Quantity" },
+                values: new object[] { 1002, "This is a Office Application", "Microsoft Office", 20.989999999999998, 50 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Quantity" },
+                values: new object[] { 1003, "The mouse that works on all surface", "Lazer Mouse", 12.02, 20 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Quantity" },
+                values: new object[] { 1004, "To store 256GB of data", "USB Storage", 5.0, 20 });
+        }
+    }
+}
